acafda5c468bd59f4819a8b3e2e15064738d6d25
lmiguelg <lmiguelgoncalves97@hotmail.com>
2019-01-02 13:04:51 +0000

    qwqqw

--- grupo7Project/resources/views/auth/login.blade.php
+++ grupo7Project/resources/views/auth/login.blade.php
@@ -1,7 +1,4 @@
-@extends('layouts.app')
 
-
-@section('content')
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -99,4 +96,3 @@
 </div>
 
 </body>
-@endsection
