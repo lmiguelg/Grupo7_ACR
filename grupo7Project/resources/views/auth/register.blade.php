d1d7556ef962b2b437b973009a3e0f62c8e42c43
lmiguelg <lmiguelgoncalves97@hotmail.com>
2019-01-02 17:44:04 +0000

    Update register.blade.php

--- grupo7Project/resources/views/auth/register.blade.php
+++ grupo7Project/resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="form">
-                <div class="header">{{ __('Register') }}</div>
+                <div class="header">Registo</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
